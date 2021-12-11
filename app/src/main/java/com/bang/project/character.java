9bf52f5142c86432305041d27203519538a582fb
방상혁 <123qkdtkdgur@naver.com>
2021-12-11 17:53:05 +0900

    앱 xml (2)

--- app/src/main/java/com/bang/project/character.java
+++ app/src/main/java/com/bang/project/character.java
@@ -3,12 +3,21 @@ package com.bang.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class character extends AppCompatActivity {
+
+    FrameLayout frameLayout;
+    BottomNavigationView bnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character);
+
+        frameLayout = findViewById(R.id.layout);
+        bnView = findViewById(R.id.bnView);
     }
 }
