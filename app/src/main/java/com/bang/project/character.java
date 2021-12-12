7c125b35dadfd44c96fe057811b2a3c9bfdacba3
moon <xaria21@naver.com>
2021-12-12 12:58:37 +0900

    - character.java 수정(임문혁)

--- app/src/main/java/com/bang/project/character.java
+++ app/src/main/java/com/bang/project/character.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,6 +13,9 @@ public class character extends AppCompatActivity {
     FrameLayout frameLayout;
     BottomNavigationView bnView;
 
+    TextView tv_nick;
+    TextView tv_level;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,13 @@ public class character extends AppCompatActivity {
 
         frameLayout = findViewById(R.id.layout);
         bnView = findViewById(R.id.bnView);
+        tv_nick = findViewById(R.id.tv_nick);
+        tv_level = findViewById(R.id.tv_level);
+
+        String nick = getIntent().getExtras().getString("nickname");
+        tv_nick.setText(nick);
+
+
+
     }
 }
