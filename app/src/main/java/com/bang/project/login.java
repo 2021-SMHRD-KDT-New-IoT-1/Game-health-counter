0777094c36a16ef70c06722c38dd299e425c8b95
moon <xaria21@naver.com>
2021-12-14 23:10:43 +0900

    - login.java 수정(임문혁)

--- app/src/main/java/com/bang/project/login.java
+++ app/src/main/java/com/bang/project/login.java
@@ -3,7 +3,9 @@ package com.bang.project;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -49,42 +51,46 @@ public class login extends AppCompatActivity {
         btn_login = findViewById(R.id.btn_login);
         btn_join = findViewById(R.id.btn_join);
 
+        // SharedPreferences 초기화
+        SharedPreferences spf = getSharedPreferences("loginSPF", Context.MODE_PRIVATE);
+
         // 1. 통로생성
         requestQueue = Volley.newRequestQueue(getApplicationContext());
-
         // 2. 전송할 URL
         String url_login = "http://211.48.213.139:8081/final_project2/Login";
 
         stringRequest_login = new StringRequest(Request.Method.POST, url_login, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                if(response.equals("")){
-                    Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 확인해주세요", Toast.LENGTH_LONG).show();
-                }else{
-                    // response를 자바객체로 파싱 메서드 호출
+                if(response.equals("성공")){
+                    // 사용자 아이디 가져오기(안드 내부에서)
+                    String user = edt_id.getText().toString();
 
+                    // SharedPreferences 에디터 열어서 값 put해주기
+                    SharedPreferences.Editor edit = spf.edit();
+                    edit.putString("user", user);
+                    edit.commit();
 
-//                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-//                    processResponse(response);
+                    Toast.makeText(getApplicationContext(), user+"님 환영합니다!", Toast.LENGTH_LONG).show();
 
-                    Toast.makeText(getApplicationContext(), response+"님 환영합니다!", Toast.LENGTH_LONG).show();
+                    // intent로 화면 넘기기
                     Intent intent = new Intent(login.this, character.class);
-
-//                    intent.putExtra("test",response);
-//                    intent.putExtra("char_name",char_vo.getC_name());
-//                    intent.putExtra("char_lv",char_vo.getC_level());
                     startActivity(intent);
+                    // response를 자바객체로 파싱 메서드 호출
+//                    processResponse(response);
+                }else{
+                    Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 확인해주세요", Toast.LENGTH_LONG).show();
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.v("asdf", error.toString());
+                Log.v("LoginError", error.toString());
             }
         }){
             @Nullable
             @Override
-            // 안드에서 패러미터를 이용하여 jsp 서블릿(Login.java)으로  요청 하는 부분
+            // 안드에서 패러미터를 이용하여 이클립스 자바 서블릿(Login.java)으로 요청하는 부분
             protected Map<String, String> getParams() throws AuthFailureError {
 
                 HashMap<String, String> params = new HashMap<>();
