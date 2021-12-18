692f2433dfc9e2deb13204e55eeca4e144851a2a
shj <shj041018@gmail.com>
2021-12-18 18:08:41 +0900

    스탯창연결해주세요ㅠㅠ

--- app/src/main/java/com/bang/project/Fragment5.java
+++ app/src/main/java/com/bang/project/Fragment5.java
@@ -1,5 +1,6 @@
 package com.bang.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 // 캐릭터 탭(메인화면)
 public class Fragment5 extends Fragment {
-
+    private ImageView main_char;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_5, container, false);
+        View v = inflater.inflate(R.layout.fragment_5, container, false);
+
+        main_char = v.findViewById(R.id.main_char);
+        main_char.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        return v;
     }
 }
--- app/src/main/java/com/bang/project/character.java
+++ app/src/main/java/com/bang/project/character.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
